23dfcd87e8d44517a951c98f4bf7bca0626dee62
Attila Csala <attilacsala@gmail.com>
2015-02-18 18:39:45 +0100

    Whitelist.class added

---
+++ 2015/week_1/Chapter_1/Whitelist.java
@@ -0,0 +1,15 @@
+public class Whitelist
+{
+   public static void main(String[] args)
+   {
+      int[] w = In.readInts(args[0]);
+      StaticSETofInts set = new StaticSETofInts(w);
+      while (!StdIn.isEmpty())
+      {  // Read key, print if not in whitelist.
+         int key = StdIn.readInt();
+         if (set.rank(key) == -1)
+         	StdOut.println(key);
+			} 
+	}
+}
+
